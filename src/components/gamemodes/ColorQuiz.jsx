9d056998bb1e69be5560c3391d69be1226f1b58b
berke <cydex201209@gmail.com>
2022-12-14 03:14:34 +0300

    gamemodes

---
+++ src/components/gamemodes/ColorQuiz.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ColorQuiz() {
+  return <div>ColorQuiz</div>;
+}
+
+export default ColorQuiz;
---
+++ src/components/gamemodes/Endless.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Endless() {
+  return <div>Endless</div>;
+}
+
+export default Endless;
